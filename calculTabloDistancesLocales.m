84879d56b09fe44491f1d23ffaed378ebbcc3e94
Nicolas Blin <blin5u@fst-ats-319-l01.fst.site.univ-lorraine.fr>
2016-11-02 13:17:22 +0100

    bellman finished

--- calculTabloDistancesLocales.m
+++ calculTabloDistancesLocales.m
@@ -1,5 +1,8 @@
 function D=calculTabloDistancesLocales(a,b,d)
 
+min1 = 0;
+min2 = 0;
+min3 = 0;
 for i = 1:length(a)
     for j = 1:length(b)
         if i==1 && j==1
@@ -11,8 +14,8 @@ for i = 1:length(a)
         else
             min1 = min(D(i-1,j-1),D(i-1,j));
             min2 = min(D(i-1,j),D(i,j-1));
-            min = min(min1,min2);
-            D(i,j) = d(i,j) + min;
+            min3 = min(min1,min2);
+            D(i,j) = d(i,j) + min3;
         end
     end
 end
